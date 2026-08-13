ee008460d9009b2411cb3f07f3a5dbd715f8ca8b
y-ygo <vdapostol2030@clc.pshs.edu.ph>
2026-08-13 08:50:28 +0800

    Done
    Refactor zodiac sign logic to correct conditions and ensure proper input handling.

--- Q1/sg2activity3.py
+++ Q1/sg2activity3.py
@@ -1,41 +1,42 @@
-x = input(" Enter your birth year: ")
+x = int(input(" Enter your birth year: "))
 
 while True:
   if x < 1900:
     print("\n Invalid Year, it should not be earlier than 1900")
-  elif x % 12 == 0:
-    print("\n Your Chinese Zodiac Sign is : Rat (鼠 / Shǔ)")
-    break
-  elif x % 12 == 1:
-    print:("\n Your Chinese Zodiac Sign is : Ox (牛 / Niú)")
-    break
-  elif x % 12 == 2:
-    print(" Your Chinese Zodiac Sign is : Tiger (虎 / Hǔ)")
-    break
-  elif x % 12 == 3:
-    print(" Your Chinese Zodiac Sign is : Rabbit (兔 / Tù)")
     break
   elif x % 12 == 4:
-    print(" Your Chinese Zodiac Sign is : Dragon (龙 / Lóng)")
+    print("\n Your Chinese Zodiac Sign is : Rat (鼠 / Shǔ)")
     break
   elif x % 12 == 5:
-    print(" Your Chinese Zodiac Sign is : Snake (蛇 / Shé)")
+    print("\n Your Chinese Zodiac Sign is : Ox (牛 / Niú)")
     break
   elif x % 12 == 6:
-    print(" Your Chinese Zodiac Sign is : Horse (马 / Mǎ)")
+    print(" Your Chinese Zodiac Sign is : Tiger (虎 / Hǔ)")
     break
   elif x % 12 == 7:
-    print(" Your Chinese Zodiac Sign is : Goat (羊 / Yáng)")
+    print(" Your Chinese Zodiac Sign is : Rabbit (兔 / Tù)")
     break
   elif x % 12 == 8:
-    print(" Your Chinese Zodiac Sign is : Monkey (猴 / Hóu)")
+    print(" Your Chinese Zodiac Sign is : Dragon (龙 / Lóng)")
     break
   elif x % 12 == 9:
-    print(" Your Chinese Zodiac Sign is : Rooster (鸡 / Jī)")
+    print(" Your Chinese Zodiac Sign is : Snake (蛇 / Shé)")
     break
   elif x % 12 == 10:
-    print(" Your Chinese Zodiac Sign is : Dog (狗 / Gǒu)")
+    print(" Your Chinese Zodiac Sign is : Horse (马 / Mǎ)")
     break
   elif x % 12 == 11:
+    print(" Your Chinese Zodiac Sign is : Goat (羊 / Yáng)")
+    break
+  elif x % 12 == 0:
+    print(" Your Chinese Zodiac Sign is : Monkey (猴 / Hóu)")
+    break
+  elif x % 12 == 1:
+    print(" Your Chinese Zodiac Sign is : Rooster (鸡 / Jī)")
+    break
+  elif x % 12 == 2:
+    print(" Your Chinese Zodiac Sign is : Dog (狗 / Gǒu)")
+    break
+  elif x % 12 == 3:
     print(" Your Chinese Zodiac Sign is : Pig (猪 / Zhū)")
     break
