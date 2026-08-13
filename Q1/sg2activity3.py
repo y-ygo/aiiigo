d4e6dfc74fcea6fbfb287786a8b594bd2b54c59f
y-ygo <vdapostol2030@clc.pshs.edu.ph>
2026-08-13 08:27:45 +0800

    Done

--- Q1/sg2activity3.py
+++ Q1/sg2activity3.py
@@ -2,7 +2,7 @@ x = input(" Enter your birth year: ")
 
 while True:
   if x < 1900:
-    print("\n Invalid Year, it should not be earlier than 1800")
+    print("\n Invalid Year, it should not be earlier than 1900")
   elif x % 12 == 0:
     print("\n Your Chinese Zodiac Sign is : Rat (鼠 / Shǔ)")
     break
@@ -15,4 +15,27 @@ while True:
   elif x % 12 == 3:
     print(" Your Chinese Zodiac Sign is : Rabbit (兔 / Tù)")
     break
-  #x%12==4
+  elif x % 12 == 4:
+    print(" Your Chinese Zodiac Sign is : Dragon (龙 / Lóng)")
+    break
+  elif x % 12 == 5:
+    print(" Your Chinese Zodiac Sign is : Snake (蛇 / Shé)")
+    break
+  elif x % 12 == 6:
+    print(" Your Chinese Zodiac Sign is : Horse (马 / Mǎ)")
+    break
+  elif x % 12 == 7:
+    print(" Your Chinese Zodiac Sign is : Goat (羊 / Yáng)")
+    break
+  elif x % 12 == 8:
+    print(" Your Chinese Zodiac Sign is : Monkey (猴 / Hóu)")
+    break
+  elif x % 12 == 9:
+    print(" Your Chinese Zodiac Sign is : Rooster (鸡 / Jī)")
+    break
+  elif x % 12 == 10:
+    print(" Your Chinese Zodiac Sign is : Dog (狗 / Gǒu)")
+    break
+  elif x % 12 == 11:
+    print(" Your Chinese Zodiac Sign is : Pig (猪 / Zhū)")
+    break
