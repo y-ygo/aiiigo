f2bae1080b934c2a137d99f4dd13805c3fea0471
y-ygo <vdapostol2030@clc.pshs.edu.ph>
2026-08-13 08:16:22 +0800

    W.I.P
    Fix print statements for Chinese Zodiac signs.

--- Q1/sg2activity3.py
+++ Q1/sg2activity3.py
@@ -4,4 +4,15 @@ while True:
   if x < 1900:
     print("\n Invalid Year, it should not be earlier than 1800")
   elif x % 12 == 0:
-    print("\n Your Chinese Zodiac Sign is : 
+    print("\n Your Chinese Zodiac Sign is : Rat (鼠 / Shǔ)")
+    break
+  elif x % 12 == 1:
+    print:("\n Your Chinese Zodiac Sign is : Ox (牛 / Niú)")
+    break
+  elif x % 12 == 2:
+    print(" Your Chinese Zodiac Sign is : Tiger (虎 / Hǔ)")
+    break
+  elif x % 12 == 3:
+    print(" Your Chinese Zodiac Sign is : Rabbit (兔 / Tù)")
+    break
+  #x%12==4
