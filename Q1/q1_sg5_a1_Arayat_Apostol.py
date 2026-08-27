f2740d3e284e8f007e8b80577fb17c6de4102836
y-ygo <vdapostol2030@clc.pshs.edu.ph>
2026-08-27 22:43:38 +0800

    Update q1_sg5_a1_Arayat_Apostol.py

--- Q1/q1_sg5_a1_Arayat_Apostol.py
+++ Q1/q1_sg5_a1_Arayat_Apostol.py
@@ -1,6 +1,13 @@
 class Hero:
-    def __init__(self, hero, hp = 100)
-    self.hero = hero
-    self.hp = hp
-    def take_amount(amount):
+    def __init__(self, name, hp = 100):
+        self.name = name
+        self.hp = hp
+    def take_damage(self, amount):
         self.hp -= amount
+Arthur = Hero("Arthur")
+Morgana = Hero("Morgana")
+
+Arthur.take_damage(10)
+
+print(f"{Arthur.name} HP: {Arthur.hp}.")
+print(f"{Morgana.name} HP: {Morgana.hp}.")
