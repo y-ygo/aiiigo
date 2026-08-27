543859fc4d4fad58a9f723cff203af0f68d4776b
y-ygo <vdapostol2030@clc.pshs.edu.ph>
2026-08-27 08:41:04 +0800

    Update q1_mycar_arayat_Apostol.py

--- Q1/q1_mycar_arayat_Apostol.py
+++ Q1/q1_mycar_arayat_Apostol.py
@@ -6,16 +6,29 @@ class Car:
         self.battery = battery
     def go(self, distance):
         s = distance/25
-        self.baterry -= s
+        self.battery -= s
         print("You have traveled", distance, "km.")
-        print("You have", self.battery, "wH left".)
+        print("Your",self.brand, self.model, "has", self.battery, "wH left.")
     def charge(self, wH):
         self.battery += wH
-        print("You charged", self.battery, "wH l\left")
+        print("Your",self.brand, self.model, "has", self.battery, "wH left.")
 
-myCar = Car("BYD", "Seal 5")
-myCar.go(100)
-myCar.charge(1)
+brand = input("What is the brand of your car?: ")
+model = input("What is the model of your car?: ")
+total_distance = 0
+myCar = Car(brand, model)
+while myCar.battery > 0:
+    command = input("What do you want to do? (go, charge): ")
+    if command == "go":
+        distance = int(input("How far (km)?: "))
+        myCar.go(distance)
+        total_distance += distance
+    elif command == "charge":
+        wH = int(input("How much (wH)?: "))
+        myCar.charge(battery)
+    else:
+         print("Invalid command.")
+print(f"Your car ran out of battery!\nYou traveled a total of {total_distance} km.")
 
 
 
