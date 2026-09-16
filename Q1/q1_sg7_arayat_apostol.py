3ce7252213aa9b25098413fa130c78d40da5ae49
y-ygo <vdapostol2030@clc.pshs.edu.ph>
2026-09-16 23:02:42 +0800

    Update q1_sg7_arayat_apostol.py

--- Q1/q1_sg7_arayat_apostol.py
+++ Q1/q1_sg7_arayat_apostol.py
@@ -1,1 +1,15 @@
+class Glassware:
+  def __init__(self, name="Beaker Name"):
+    self.name = name
 
+class Beaker(Glassware):
+  pass
+
+class Tray:
+  def __init__(self):
+    self.beakers = []
+    for i in range(5):
+      self.beakers.append(Beaker())
+
+my_tray = Tray()
+print(f"You currently have {len(my_tray.beakers)} beakers.")
