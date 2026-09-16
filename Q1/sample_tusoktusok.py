80a7e983e29bb3f282c15d5329695fa20a24c6d8
y-ygo <vdapostol2030@clc.pshs.edu.ph>
2026-09-17 00:04:58 +0800

    Add Sauce and Tusoktusok classes with example usage

---
+++ Q1/sample_tusoktusok.py
@@ -0,0 +1,25 @@
+class Sauce:
+    def __init__(self,name,taste):
+        self.name=name
+        self.taste=taste
+        print(f"{self.name} is cooked.")
+    def __del__(self):
+        print(f"{self.name} is ubos na.")
+    def __str__(self):
+        return "This is "+self.name+" and it tastes "+self.taste+"."
+
+class Tusoktusok:
+    def __init__(self,name,sauce):
+        self.name=name
+        self.sauce=sauce
+        print(f"{self.name} is cooked and dipped in {self.sauce.name}.")
+    def eat(self):
+        print(f"I am eating {self.name} and it tastes {self.sauce.taste}.")
+    def __del__(self):
+        print(f"{self.name} was thrown in the trash can.")
+
+vinegar=Sauce("Vinegar","sour")
+fishball=Tusoktusok("fishball",vinegar)
+fishball.eat()
+del fishball
+print(vinegar)
